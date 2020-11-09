5113a8318002d3f932e0f9a52223596fbf30be35
Alfred <alc9020@nyp.org>
2020-11-09 21:44:03 +0000

    your commit message

---
+++ dict01/disct01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def main():
+    ## create a dictionary
+    switch = {'hostname': 'sw-01', 'ip': '10.0.1.1', 'version': '1.2', 'vendor': 'cisco'}
+    
+    ## display parts of the dictionary
+    print( switch['hostname'] )
+    print( switch['ip'] )
+    
+    ## request a 'fake' key
+    print( switch['lynx'] )
+    
+main()
+

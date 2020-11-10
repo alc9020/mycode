adb5b835ab1170811d6df1b8f67663a9411e1ac6
Alfred <alc9020@nyp.org>
2020-11-10 16:17:00 +0000

    Committed

---
+++ if-test2/iftest2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+ipchk = '192.168.0.1'
+
+if ipchk:
+   print('Looks like the IP address was set: ' + ipchk)
+#!/usr/bin/env python3
+ipchk = input('Apply an IP address: ') # this line now prompts the user for input
+
+if ipchk: # if any data is provided, this will test true
+   print('Looks like the IP address was set: ' + ipchk) # indented under if
+#!/usr/bin/env python3
+ipchk = input('Apply an IP address: ') # this line now prompts the user for input
+
+if ipchk: # if any data is provided, this will test true
+   print('Looks like the IP address was set: ' + ipchk) # indented under if
+else: # if data is NOT provided
+   print('You did not provide input.') # indented under else
+#!/usr/bin/env python3
+ipchk = '192.168.0.1'
+
+if ipchk:
+   print('Looks like the IP address was set: ' + ipchk)
+#!/usr/bin/env python3
+ipchk = input('Apply an IP address: ') # this line now prompts the user for input
+
+if ipchk: # if any data is provided, this will test true
+   print('Looks like the IP address was set: ' + ipchk) # indented under if
+#!/usr/bin/env python3
+ipchk = input('Apply an IP address: ') # this line now prompts the user for input
+
+if ipchk: # if any data is provided, this will test true
+   print('Looks like the IP address was set: ' + ipchk) # indented under if
+else: # if data is NOT provided
+   print('You did not provide input.') # indented under else
